26ef4d0be8caf15215ff8747daf7d5f9274f2482
Rune Johansen <runejo@gmail.com>
2019-10-15 23:55:05 +0200

    terraform fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,25 +2,25 @@ variable "client_id" {}
 variable "client_secret" {}
 
 variable "agent_count" {
-    default = 3
+  default = 3
 }
 
 variable "ssh_public_key" {
-    default = "~/.ssh/id_rsa.pub"
+  default = "~/.ssh/id_rsa.pub"
 }
 
 variable "dns_prefix" {
-    default = "k8stest"
+  default = "k8stest"
 }
 
 variable cluster_name {
-    default = "k8stest"
+  default = "k8stest"
 }
 
 variable resource_group_name {
-    default = "azure-k8stest"
+  default = "azure-k8stest"
 }
 
 variable location {
-    default = "North Europe"
+  default = "North Europe"
 }
