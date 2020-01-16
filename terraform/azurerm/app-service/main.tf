c6ac8ef2934b4ba50843f4ef726a42364c0e3171
Rune Johansen <runejo@gmail.com>
2020-01-16 19:10:31 +0100

    Move to one singe Terraform file

--- terraform/azurerm/app-service/main.tf
+++ terraform/azurerm/app-service/main.tf
@@ -1,12 +1,17 @@
+variable "prefix" {
+  description = "The prefix used for all resources in this example"
+  default     = "pxweb-docker"
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resources"
-  location = "${var.location}"
+  location = "westeurope"
 }
 
 resource "azurerm_app_service_plan" "example" {
   name                = "${var.prefix}-asp"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   kind                = "xenon"
   is_xenon            = true
 
@@ -18,9 +23,9 @@ resource "azurerm_app_service_plan" "example" {
 
 resource "azurerm_app_service" "example" {
   name                = "${var.prefix}-appservice"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.example.id}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.name
 
   site_config {
     windows_fx_version = "DOCKER|runejo/pxweb:windowsservercore-1803"
@@ -28,6 +33,9 @@ resource "azurerm_app_service" "example" {
 
   app_settings = {
     "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io",
-
   }
+}
+
+output "website_url" {
+  value = azurerm_app_service.example.default_site_hostname
 }
--- terraform/azurerm/app-service/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "prefix" {
-  description = "The prefix used for all resources in this example"
-  default = "pxweb"
-}
-
-variable "location" {
-  description = "The Azure location where all resources in this example should be created"
-  default     = "North Europe"
-}
